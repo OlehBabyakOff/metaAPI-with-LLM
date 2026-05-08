da0a7e4e6f6e20a8d96fe33f263ca7e109ab86be
Oleh Babyak <babyakwork@gmail.com>
2026-05-08 18:07:49 +0300

    rule upd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-misused-promises': 'error',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-assignment': 'warn',
