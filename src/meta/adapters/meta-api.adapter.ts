569536d816df8d5bd8614315978f6f73ef2095e8
Oleh Babyak <babyakwork@gmail.com>
2026-05-09 23:01:08 +0300

    added batch request for pages description

--- src/meta/adapters/meta-api.adapter.ts
+++ src/meta/adapters/meta-api.adapter.ts
@@ -28,6 +28,11 @@ interface MetaOAuthUrlParams {
   state: string;
 }
 
+interface BatchResponse {
+  code: number;
+  body: string;
+}
+
 export interface MetaPageResult {
   pageId: string;
   name: string;
@@ -38,6 +43,7 @@ export interface MetaPageResult {
 
 @Injectable()
 export class MetaApiAdapter {
+  private readonly batchUrl = 'https://graph.facebook.com';
   private readonly baseUrl = 'https://graph.facebook.com/v25.0';
   private readonly oauthBaseUrl = 'https://www.facebook.com/v25.0/dialog/oauth';
   private readonly tokenUrl = 'https://graph.facebook.com/v25.0/oauth/access_token';
@@ -84,19 +90,15 @@ export class MetaApiAdapter {
 
   // Pages methods
   async getUserPages(accessToken: string): Promise<MetaPageResult[]> {
-    const response = await this.http.get<RawMetaPageListResponse>(`${this.baseUrl}/me/accounts`, {
-      params: {
-        access_token: accessToken,
-        fields: 'id,name,description,category,access_token',
-      },
-    });
+    const pages = await this.getPages(accessToken);
+    const descriptions = await this.getDescriptionsBatch(pages, accessToken);
 
-    return response.data.map((page) => ({
+    return pages.map((page, i) => ({
       pageId: page.id,
       name: page.name,
       category: page.category ?? 'Business',
       accessToken: page.access_token,
-      description: page.description,
+      description: descriptions[i],
     }));
   }
 
@@ -110,5 +112,50 @@ export class MetaApiAdapter {
       { description },
       { params: { access_token: pageAccessToken } },
     );
+  }
+
+  private async getPages(accessToken: string): Promise<RawMetaPage[]> {
+    const response = await this.http.get<RawMetaPageListResponse>(`${this.baseUrl}/me/accounts`, {
+      params: {
+        access_token: accessToken,
+        fields: 'id,name,category,access_token',
+      },
+    });
+
+    return response.data;
+  }
+
+  private async getDescriptionsBatch(
+    pages: RawMetaPage[],
+    accessToken: string,
+  ): Promise<(string | undefined)[]> {
+    if (!pages.length) {
+      return [];
+    }
+
+    const batch = pages.map((page) => ({
+      method: 'GET',
+      relative_url: `${page.id}?fields=description&access_token=${page.access_token}`,
+    }));
+
+    const results = await this.http.post<BatchResponse[]>(this.batchUrl, null, {
+      params: {
+        access_token: accessToken,
+        batch: JSON.stringify(batch),
+        include_headers: false,
+      },
+    });
+
+    const parsedResults = results.map((item) => {
+      if (item.code !== 200 || !item.body) {
+        return undefined;
+      }
+
+      const body = JSON.parse(item.body);
+
+      return body.description;
+    });
+
+    return parsedResults;
   }
 }
