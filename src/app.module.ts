fd1fb03f18c2188d2c2bedc9863e8818a381a2c8
Oleh Babyak <babyakwork@gmail.com>
2026-05-09 18:04:05 +0300

    updated modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/config.module';
 import { LlmModule } from './llm/llm.module';
+import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [AppConfigModule, LlmModule],
+  imports: [AppConfigModule, LlmModule, HttpModule],
   controllers: [],
   providers: [],
 })
