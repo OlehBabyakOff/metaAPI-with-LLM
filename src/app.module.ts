436ae26885c32c0de07b20731e69e412641ecd90
Oleh Babyak <babyakwork@gmail.com>
2026-05-11 22:36:11 +0300

    serve static in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { AppConfigModule } from './config/config.module';
 import { LlmModule } from './llm/llm.module';
 import { HttpModule } from './http/http.module';
@@ -7,7 +10,18 @@ import { DatabaseModule } from './database/database.module';
 import { PagesModule } from './pages/pages.module';
 
 @Module({
-  imports: [AppConfigModule, LlmModule, HttpModule, MetaModule, DatabaseModule, PagesModule],
+  imports: [
+    AppConfigModule,
+    LlmModule,
+    HttpModule,
+    MetaModule,
+    DatabaseModule,
+    PagesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/*', '/auth/*'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
