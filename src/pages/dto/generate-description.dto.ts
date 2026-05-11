0754768945ee672e14b5374783e310cdefa2a9a2
Oleh Babyak <babyakwork@gmail.com>
2026-05-11 22:35:18 +0300

    updated dto

--- src/pages/dto/generate-description.dto.ts
+++ src/pages/dto/generate-description.dto.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class GenerateDescriptionDTO {
   @ApiPropertyOptional({
@@ -8,6 +8,5 @@ export class GenerateDescriptionDTO {
   })
   @IsOptional()
   @IsString()
-  @MinLength(10)
   customPrompt?: string;
 }
