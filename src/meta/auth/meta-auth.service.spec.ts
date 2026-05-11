1b38d60b030e55c11f795dab89ffdf640b215e32
Oleh Babyak <babyakwork@gmail.com>
2026-05-11 22:37:08 +0300

    updated tests

--- src/meta/auth/meta-auth.service.spec.ts
+++ src/meta/auth/meta-auth.service.spec.ts
@@ -60,7 +60,7 @@ describe('MetaAuthService', () => {
       expect(mockAdapter.buildOAuthUrl).toHaveBeenCalledWith({
         clientId: 'test-app-id',
         redirectUri: 'http://localhost:3000/auth/meta/callback',
-        scopes: ['pages_show_list', 'pages_manage_metadata'],
+        scopes: ['pages_show_list', 'pages_manage_metadata', 'pages_read_engagement'],
         state,
       });
     });
